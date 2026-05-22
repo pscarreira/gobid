cb4e78fb5252bf2f8fb9429d9fa5ad1581ca33ea
Paulo Carreira <psalvescarreira@gmail.com>
2026-05-22 14:09:53 -0300

    feat: adicionar rotas e manipuladores de usuários com Chi

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/pscarreira/gobid
 
 go 1.26.2
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi/v5 v5.3.0
+	github.com/joho/godotenv v1.5.1
+)
---
+++ internal/api/users_handlers.go
@@ -0,0 +1,15 @@
+package api
+
+import "net/http"
+
+func (api *Api) handleSignUpUser(w http.ResponseWriter, r *http.Request) {
+	panic("Todo: Implement handleSignUpUser")
+}
+
+func (api *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+	panic("Todo: Implement handleLoginUser")
+}
+
+func (api *Api) handleLogoutUser(w http.ResponseWriter, r *http.Request) {
+	panic("Todo: Implement handleLogoutUser")
+}
